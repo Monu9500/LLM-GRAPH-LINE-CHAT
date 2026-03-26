bd840340aff37e3d539c3d9fca4fe2be0e1b7ff4
Pritam Ghosh <pritamghoshdhamsa@gmail.com>
2026-03-26 19:00:59 +0530

    Update

--- backend/database.py
+++ backend/database.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 URI = "neo4j://127.0.0.1:7687"
 USERNAME = "neo4j"
-PASSWORD = "Pritam@123"
+PASSWORD = "w842g0vi7gKSZMIPeLIA5AzEvohmGfgNpf72frqEoY8"
 
 driver = GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD))
 
 
 def run_query(query, parameters=None):
-    with driver.session(database="fde-db") as session:
+    with driver.session(database="neo4j") as session:
         result = session.run(query, parameters)
         return [record.data() for record in result]
         
